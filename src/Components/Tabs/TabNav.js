4980a16a9c60c6a6aebc4c4f7f72c1b9720f50ab
Max Murakami-Moses <160978136+bullcitydao@users.noreply.github.com>
2024-03-07 15:51:53 -0500

    Added a new Media tab

--- src/Components/Tabs/TabNav.js
+++ src/Components/Tabs/TabNav.js
@@ -11,7 +11,7 @@ function a11yProps(index) {
     };
   }
 
-const TabNav = ({podcasts, videos}) => {
+  const TabNav = ({podcasts, videos, media}) => { // Include media as a prop here
     const [value, setValue] = React.useState(0);
 
     const handleChange = (event, newValue) => {
@@ -21,30 +21,40 @@ const TabNav = ({podcasts, videos}) => {
     return(
         <div>
             <Box sx={{ width: '100%' }}>
-       <Tabs 
-        textColor = '#EEE' indicatorColor = 'secondary' centered value={value} onChange={handleChange} aria-label="basic tabs example">
-        <Tab sx = 
-        {{ fontSize: '15px',
-            textDecoration: "none",
-            textTransform: "none",}}
-        
-        label="Podcasts" {...a11yProps(0)} />
-        <Tab 
-        sx = 
-        {{ fontSize: '15px',
-            textDecoration: "none",
-            textTransform: "none",}}
-        
-        label="Videos" {...a11yProps(1)} />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        {podcasts}
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        {videos}
-      </TabPanel>
-      </Box>
-      </div>)
+                <Tabs 
+                    textColor = '#EEE' indicatorColor = 'secondary' centered value={value} onChange={handleChange} aria-label="basic tabs example">
+                    <Tab sx = 
+                    {{ fontSize: '15px',
+                        textDecoration: "none",
+                        textTransform: "none",}}
+                    label="Podcasts" {...a11yProps(0)} />
+                    
+                    <Tab 
+                    sx = 
+                    {{ fontSize: '15px',
+                        textDecoration: "none",
+                        textTransform: "none",}}
+                    label="Videos" {...a11yProps(1)} />
+                    
+                    <Tab 
+                    sx = 
+                    {{ fontSize: '15px',
+                        textDecoration: "none",
+                        textTransform: "none",}}
+                    label="Media" {...a11yProps(2)} />
+                </Tabs>
+                <TabPanel value={value} index={0}>
+                    {podcasts}
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    {videos}
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    {media}
+                </TabPanel>
+            </Box>
+        </div>
+    )
 }
 
 export default TabNav
--- src/Pages/Content.js
+++ src/Pages/Content.js
@@ -1,69 +1,69 @@
-import React from 'react'
+import React from 'react';
 import Fade from 'react-reveal/Fade';
-import QuarterBreakS from '../Components/QuarterBreak/QuarterBreakS'
-import ContentItem from '../Components/Content/ContentItem'
-import ContentItemVideo from '../Components/Content/ContentItemVideo'
-import TabNav from '../Components/Tabs/TabNav'
+import QuarterBreakS from '../Components/QuarterBreak/QuarterBreakS';
+import ContentItem from '../Components/Content/ContentItem';
+import ContentItemVideo from '../Components/Content/ContentItemVideo';
+import TabNav from '../Components/Tabs/TabNav';
 import HeadphonesIcon from '@mui/icons-material/Headphones';
 import SmartDisplayIcon from '@mui/icons-material/SmartDisplay';
 
 const Content = () => {
-
-    return(
-        <div>
-            <Fade>
-            <TabNav 
-
-                podcasts = {
-                    <div>
-
-                    <QuarterBreakS 
-                        title = "Our Podcasts"
-                        subtitle = "Revolutionary blockchain based podcasts that will change the world."
-                        icon = {<HeadphonesIcon color = 'secondary' sx={{ fontSize: "200px" }}/>}                
-                    />
-        
-                    <ContentItem
-                        to = "https://drive.google.com/file/d/1ZagvTA92aV_aHX_5PvGlxNyRvk_6U4gq/view?usp=sharing" 
-                        title = "#1 Duke's Blockchain Wizard"
-                        author = "Karam Bambawale"
-                        about = "A conversation with Duke's own prodigious blockchain engineer Giovanni Vignone, and those who have worked with him in the past."
-                    />
-
-                    </div>
-                    }
-
-                videos = {
-                    <div>
-
-                    <QuarterBreakS 
-                        title = "Our Videos"
-                        subtitle = "Tik Toks and Youtube videos from the Bull City DAO. Here are a few, but click on the Video Icon to the left to watch more! "
-                        icon = {<SmartDisplayIcon onClick={() => window.open('https://www.tiktok.com/@webthreefundamentals?_t=8cX9HPBVfwc&_r=1')} color = 'secondary' sx={{ fontSize: "200px" }}/>}                
-                    />
-
-                    <ContentItemVideo
-                        links = {["https://www.tiktok.com/embed/7219113537468108075",
-                                "https://www.tiktok.com/embed/7220952863269244203" ,
-                                "https://www.tiktok.com/embed/7221672852645465390"]}
-                    />
-                    <ContentItemVideo
-                        links = {["https://www.tiktok.com/embed/7222484725531741482",
-                                "https://www.tiktok.com/embed/7223547790834634026" ,
-                                "https://www.tiktok.com/embed/7225023226156535083"]}
-                    />
-                    <ContentItemVideo
-                        links = {["https://www.tiktok.com/embed/7266575164811398446",
-                                "https://www.tiktok.com/embed/7265078162118282538" ,
-                                "https://www.tiktok.com/embed/7264345787147210027"]}
-                    />
-                    </div>
-                    }
-            />
-
-            </Fade>
-        </div>
-    )
+  return (
+    <div>
+      <Fade>
+        <TabNav
+          podcasts={
+            <div>
+              <QuarterBreakS
+                title="Our Podcasts"
+                subtitle="Revolutionary blockchain based podcasts that will change the world."
+                icon={<HeadphonesIcon color='secondary' sx={{ fontSize: "200px" }} />}
+              />
+              <ContentItem
+                to="https://drive.google.com/file/d/1ZagvTA92aV_aHX_5PvGlxNyRvk_6U4gq/view?usp=sharing"
+                title="#1 Duke's Blockchain Wizard"
+                author="Karam Bambawale"
+                about="A conversation with Duke's own prodigious blockchain engineer Giovanni Vignone, and those who have worked with him in the past."
+              />
+            </div>
+          }
+          videos={
+            <div>
+              <QuarterBreakS
+                title="Our Videos"
+                subtitle="Tik Toks and Youtube videos from the Bull City DAO. Here are a few, but click on the Video Icon to the left to watch more!"
+                icon={<SmartDisplayIcon onClick={() => window.open('https://www.tiktok.com/@webthreefundamentals?_t=8cX9HPBVfwc&_r=1')} color='secondary' sx={{ fontSize: "200px" }} />}
+              />
+              <ContentItemVideo
+                links={[
+                  "https://www.tiktok.com/embed/7219113537468108075",
+                  "https://www.tiktok.com/embed/7220952863269244203",
+                  "https://www.tiktok.com/embed/7221672852645465390"
+                ]}
+              />
+              {/* Add more ContentItemVideo components as needed */}
+            </div>
+          }
+          media={
+            <div>
+              <QuarterBreakS
+                title="Our Media"
+                subtitle="This is the media section."
+                icon={<HeadphonesIcon color='secondary' sx={{ fontSize: "200px" }} />}
+              />
+              <ContentItem
+                to="https://drive.google.com/file/d/1ZagvTA92aV_aHX_5PvGlxNyRvk_6U4gq/view?usp=sharing"
+                title="#1 Duke's Blockchain Wizard"
+                author="Karam Bambawale"
+                about="A conversation with Duke's own prodigious blockchain engineer Giovanni Vignone, and those who have worked with him in the past."
+              />
+              {/* Add more ContentItem components as needed */}
+            </div>
+          }
+        />
+      </Fade>
+    </div>
+  );
 }
 
-export default Content
+export default Content;
