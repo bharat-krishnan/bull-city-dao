a89d888c944db0c74e5a6ae9a8626a3b8063914f
Max Murakami-Moses <160978136+bullcitydao@users.noreply.github.com>
2024-03-07 22:51:31 -0500

    Updated the Incubator header

--- src/Pages/ComingSoon.js
+++ src/Pages/ComingSoon.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade';
-import Subheading from '../Components/Subheading'
+import QuarterBreakS from '../Components/QuarterBreak/QuarterBreakS'
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+
 
 const ComingSoon = () => {
 
     return(
         <div>
             <Fade>
-            <Subheading 
-                align = 'center'
-                section = '...'
-                title = 'This part of the website is still being built'
-                subtitle = 'Come back soon for updates!'
-            /> 
-        
+            <QuarterBreakS 
+                title = "Our Incubator"
+                subtitle = "Every semester, we fund and help guide the best Duke founders pursing blockchain related startups."
+                icon = {<LightbulbIcon color = 'secondary' sx={{ fontSize: "200px" }}/>}                
+            />
         </Fade>
         </div>
     )
