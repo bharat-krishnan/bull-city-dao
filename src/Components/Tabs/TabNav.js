dd5e5ca20ce196c7050126cfdab9ba834edbcf29
Max Murakami-Moses <160978136+bullcitydao@users.noreply.github.com>
2024-03-22 16:02:28 -0400

    Removed content tab
    bc tiktok videos got deleted

--- src/Components/Tabs/TabNav.js
+++ src/Components/Tabs/TabNav.js
@@ -1,60 +1,39 @@
-import React from 'react'
+import React from 'react';
 import Tabs from '@mui/material/Tabs';
-import Box from '@mui/material/Box'
+import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
-import TabPanel from './TabPanel'
+import TabPanel from './TabPanel';
 
 function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
-
-  const TabNav = ({podcasts, videos, media}) => { // Include media as a prop here
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
-
-    return(
-        <div>
-            <Box sx={{ width: '100%' }}>
-                <Tabs 
-                    textColor = '#EEE' indicatorColor = 'secondary' centered value={value} onChange={handleChange} aria-label="basic tabs example">
-                    <Tab sx = 
-                    {{ fontSize: '15px',
-                        textDecoration: "none",
-                        textTransform: "none",}}
-                    label="Podcasts" {...a11yProps(0)} />
-                    
-                    <Tab 
-                    sx = 
-                    {{ fontSize: '15px',
-                        textDecoration: "none",
-                        textTransform: "none",}}
-                    label="Videos" {...a11yProps(1)} />
-                    
-                    <Tab 
-                    sx = 
-                    {{ fontSize: '15px',
-                        textDecoration: "none",
-                        textTransform: "none",}}
-                    label="Media" {...a11yProps(2)} />
-                </Tabs>
-                <TabPanel value={value} index={0}>
-                    {podcasts}
-                </TabPanel>
-                <TabPanel value={value} index={1}>
-                    {videos}
-                </TabPanel>
-                <TabPanel value={value} index={2}>
-                    {media}
-                </TabPanel>
-            </Box>
-        </div>
-    )
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
 }
 
-export default TabNav
+const TabNav = ({podcasts, media}) => { // Removed videos from the props
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <Box sx={{ width: '100%' }}>
+        <Tabs textColor='#EEE' indicatorColor='secondary' centered value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab sx={{ fontSize: '15px', textDecoration: "none", textTransform: "none",}} label="Podcasts" {...a11yProps(0)} />
+          <Tab sx={{ fontSize: '15px', textDecoration: "none", textTransform: "none",}} label="Media" {...a11yProps(1)} />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          {podcasts}
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          {media}
+        </TabPanel>
+      </Box>
+    </div>
+  );
+}
+
+export default TabNav;
